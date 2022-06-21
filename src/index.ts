24dafe22d430491ee26e95b7cba327bfdb6c76ab
bbaldino <bbaldino@cisco.com>
2022-06-21 16:34:26 -0700

    fix: add missing exports

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from './parser';
 export * from './lines';
 export * from './model';
 export * from './munge';
+export * from './regex-helpers';
--- src/lines/index.ts
+++ src/lines/index.ts
@@ -3,6 +3,7 @@ export * from './bundle-group-line';
 export * from './connection-line';
 export * from './direction-line';
 export * from './fmtp-line';
+export * from './line';
 export * from './media-line';
 export * from './origin-line';
 export * from './rtcpfb-line';
