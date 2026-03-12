3c6cce9fe7e79adc8bb4c2ac41d7997468cc5266
bbaldino <bbaldino@cisco.com>
2026-03-12 11:22:02 -0700

    ci: fix semantic-release commit style

--- release.config.js
+++ release.config.js
@@ -17,8 +17,8 @@
 module.exports ={
   "branches": ["main"],
   "plugins": [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
+    ["@semantic-release/commit-analyzer", { "preset": "conventionalcommits" }],
+    ["@semantic-release/release-notes-generator", { "preset": "conventionalcommits" }],
     "@semantic-release/changelog",
     ["@semantic-release/npm", {
       "npmPublish": true
